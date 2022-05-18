e7060b085c16cd30b56c0d4d0c0427b56b23044b
DUMITRU MIHAELA <86972784+mihaeladumitru93@users.noreply.github.com>
2022-05-19 00:10:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// START HEADER
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+navSlide();
+// END HEADER
